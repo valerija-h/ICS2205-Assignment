fbda404642bd1578b06e3d5612d993482f16c2bb
valerija <vholomjova@gmail.com>
2018-12-11 11:30:25 +0100

    Loading and Testing Data

--- Vally/index.php
+++ Vally/index.php
@@ -1,1 +1,10 @@
-<?php echo("test");?>
+<?php
+//Loading the XML File
+$xml = simplexml_load_file("data.xml") or die("Error: Cannot create object");
+
+//Iterating through each thread.
+foreach($xml->thread as $thread) {
+    //Testing
+    echo $thread->DOC->To;
+}
+?>
