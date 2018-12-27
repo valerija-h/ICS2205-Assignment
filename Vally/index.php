bd5d2d2f18de2c581f7698c035a247017118919e
valerija <vholomjova@gmail.com>
2018-12-27 14:10:50 +0100

    Cleaned up code more - Changed variable names to be more clear.

--- Vally/index.php
+++ Vally/index.php
@@ -14,93 +14,137 @@ $documents = [];
 //Some objects that will be needed.
 class Document{
     public $senders=[];
-    public $emails=[];
-    public $final=[];
+    public $keywords=[];
+    public $keywordsFreqs=[];
+    public $keywordsWeights=[];
 }
+
+//In the keywordsWeights state, each keyword will be stored using this.
 class Keyword{
-    public $str;
-    public $freq;
+    public $word;
+    public $weight;
 }
-function addToDoc($from,$receiver,$temp){
+
+parseDocument($xml);
+print_r($documents);
+
+//Appends the keywords to an existing document or creates a new one and appends it.
+function appendDoc($from, $receiver, $keywords){
     global $documents;
     //Check if senders already exist in a doc and merge that to the array.
     foreach($documents as $document){
         //If the senders already have a document.
         if(in_array($from,$document->senders) && in_array($receiver,$document->senders)){
             //Append them to the current documents and return.
-            $document->emails = array_merge($document->emails,$temp);
+            $document->keywords = array_merge($document->keywords, $keywords);
             return;
         }
     }
-    //Otherwise make a new Document
-    $tempDoc = new Document();
-    $tempDoc->senders = [$from,$receiver];
-    $tempDoc->emails = $temp;
-    array_push($documents,$tempDoc);
-    return;
+    //Otherwise make a new Document and add it to the global array.
+    $newDoc = new Document();
+    $newDoc->senders = [$from,$receiver];
+    $newDoc->keywords = $keywords;
+    array_push($documents, $newDoc);
 }
 
-//Iterating through each thread.
-foreach($xml->thread as $thread) {
-    //Iterating through each email.
-    foreach($thread->DOC as $email){
-        // -------------- Obtaining Keywords -------------- /
-        //The messages in each email - not Quotes.
-        $text = $email->Text->content;
-        //Parse the messages by delimiters and store them in temporary array.
-        $temp = [];
-        //Tokenizer to parse text.
-        $tok = strtok($text, " ,-()\n<>");
-        while ($tok !== false) {
-            //Make word lowercase and strip '.' from end of string!
-            $tok = rtrim(strtolower($tok),'.');
-            //Check if it is a stop word - if not add to the array!
-            if (!in_array($tok, $stopWords)) {
-                //Porter Stemmer Library - gets the stem of the word.
-                $tok = PorterStemmer::Stem($tok);
-                array_push($temp, $tok);
-            }
-            $tok = strtok(" ,-()\n<>");
+function createDoc($from, $receivers, $keywords){
+    global $documents;
+    foreach($receivers as $receiver) {
+        $receiver = str_replace(array('>','<'), '', $receiver);
+        //Create first object in documents array else appends/merges a document object.
+        if(empty($documents)) {
+            $tempDoc = new Document();
+            $tempDoc->senders = [$from,$receiver];
+            $tempDoc->keywords = $keywords;
+            array_push($documents, $tempDoc);
+        } else {
+            //Either creates a new doc or adds to a previous one.
+            appendDoc($from, $receiver, $keywords);
         }
-
-        // ---------------  Choosing Appropriate Document ---------------
-        //Get the To, From AND CC
-        $from = str_replace(array('>','<'), '', $email->From);
-        //Split them by , and strip for ' ' then append them to an array of recievers.
-        $to = explode(',',$email->To);
-        //If CC exists split and strip then merge with to.
-        if(isset($email->Cc)){
-            $cc = explode(',',$email->Cc);
-            $to = array_merge($to,$cc);
-        }
-
-        foreach($to as $receiver) {
-            $receiver = str_replace(array('>','<'), '',$receiver);
-            //Create first object in documents array else append object.
-            if(empty($documents)) {
-                $tempDoc = new Document();
-                $tempDoc->senders = [$from,$receiver];
-                $tempDoc->emails = $temp;
-                array_push($documents,$tempDoc);
-            } else {
-                //Either creates a new doc or adds to a previous one.
-                addToDoc($from,$receiver,$temp);
-            }
-        }
-
-
-
-
-        //Testing print final array.
-        //print_r($temp);
-
-        //Add the PHP array to a JS Array ?>
-        <!-- <script>
-            var object = <?php echo json_encode($temp) ?>;
-            console.log(object);
-        </script> -->
-<?php
     }
 }
-print_r($documents);
+
+//Returns an array of clean key words - without stop words, unwanted characters and stemmed.
+function cleanUpMsg($message){
+    global $stopWords;
+    $keywords = [];
+    //Tokenizer to parse text.
+    $tok = strtok($message, " ,-()\n<>");
+    while ($tok !== false) {
+        //Make word lowercase and strip '.' from end of string!
+        $tok = rtrim(strtolower($tok),'.');
+        //Check if it is a stop word - if not add to the array!
+        if (!in_array($tok, $stopWords)) {
+            //Porter Stemmer Library - gets the stem of the word.
+            $tok = PorterStemmer::Stem($tok);
+            array_push($keywords, $tok);
+        }
+        $tok = strtok(" ,-()\n<>");
+    }
+    return $keywords;
+}
+
+//Parses the xml file and adds Document class objects to the global Documents array.
+function parseDocument($xml){
+    //Iterating through each thread.
+    foreach($xml->thread as $thread) {
+        //Iterating through each email.
+        foreach($thread->DOC as $email){
+            //Obtains the messages in each email - and cleans it up into an array of keywords.
+            $text = $email->Text->content;
+            $keywords = cleanUpMsg($text);
+
+            //Gets the To, From AND CC.
+            $from = str_replace(array('>','<'), '', $email->From);
+            //Split them by , and strip for ' ' then append them to an array of recievers.
+            $to = explode(',',$email->To);
+            //If CC exists split and strip then merge with the current recievers.
+            if(isset($email->Cc)){
+                $cc = explode(',',$email->Cc);
+                $to = array_merge($to,$cc);
+            }
+            createDoc($from, $to, $keywords);
+        }
+    }
+    //Adds the keywords with frequencies and then keywords with weights to each document.
+    addFreq();
+    addWeights();
+}
+
+//Adds keywords with frequencies to each document, key-value array of word and frequency in document.
+function addFreq(){
+    global $documents;
+    foreach($documents as $document){
+        $keywords = $document->keywords;
+        $document->keywordsFreqs = array_count_values($keywords);
+    }
+}
+
+//Adds keywords with weights to each document, the keywords are keyword class objects.
+function addWeights(){
+    global $documents;
+    $totalDoc = sizeof($documents);
+    foreach($documents as $document){
+       //For each word in the document calculate the weights.
+       foreach($document->keywordsFreqs as $key => $value){
+           $TFIDF = round($value * log($totalDoc/getDocNo($key)));
+           $weighted = new keyWord();
+           $weighted->word = $key;
+           $weighted->weight = $TFIDF;
+           array_push($document->keywordsWeights,$weighted);
+       }
+    }
+}
+
+//Returns number of documents containing the word.
+function getDocNo($word){
+    $docNumber = 0;
+    global $documents;
+    foreach($documents as $document){
+        if(array_key_exists($word,$document->keywordsFreqs)){
+            $docNumber++;
+        }
+    }
+    return $docNumber;
+}
 ?>
