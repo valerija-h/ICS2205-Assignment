82573c1106de7958a1fd674eb2c452dcc3a8c1c7
Gabriel <gabriel.camilleri.17@um.edu.mt>
2018-12-30 21:44:45 +0100

    passing keywords and refactored some code

--- Gabe/WordCloud/index.php
+++ Gabe/WordCloud/index.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>  
 <html>
   <script src="http://d3js.org/d3.v3.min.js"></script>
@@ -14,7 +17,56 @@
       <h1 style="text-align:center">Word Cloud</h1>
       <div class="container" style="text-align:center">
           <div class="child" style="display:inline-block">
-              <script src="script.js" ></script>
+              <script>
+			  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}				
+				const param=getParameterByName('words',window.location.search);
+			  const urlParams = new URLSearchParams(window.location.search);
+				const myParam = urlParams.get('words');
+			  const keywords=JSON.parse(param);
+			      // as attributes has text: and size:
+				var frequency_list = [];
+	
+				
+					var w = 960,
+						h = 600;
+
+					var fill = d3.scale.category20b();
+
+					d3.layout.cloud().size([w, h])
+							.words(frequency_list)
+							.rotate(0)
+							.padding(5)
+							.fontSize(function(d) { return d.size; })
+							.on("end", draw)
+							.start();
+
+					function draw(words, bounds) {
+					d3.select("body").append("svg")
+						.attr("width", w)
+						.attr("height", h)
+					  .append("g")
+						.attr("transform", "translate(450,300)")
+					  .selectAll("text")
+						.data(words)
+					  .enter().append("text")
+						.style("font-size", function(d) { return d.size + "px"; })
+						.style("font-family", "Impact")
+						.style("fill", function(d, i) { return fill(i); })
+						.attr("text-anchor", "middle")
+						.attr("transform", function(d) {
+						  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+						})
+						.text(function(d) { return d.text; })
+				  }
+			  </script>
           </div>
       </div>
   </body>
